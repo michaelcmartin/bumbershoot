961b5efff7739f9b47b21bea9ec3abbc91747c9e
Michael Martin <mcmartin@gmail.com>
2017-03-11 01:47:12 -0800

    First complete draft of LightsOut81

---
+++ zx81/lightsout81.asm
@@ -0,0 +1,345 @@
+        call    draw_board
+        ;; Seed RNG
+        ld      hl, ($4034)     ; frame count since power on
+        ld      (rnd_x), hl
+m_start:
+        ld      hl, title_wait_msg
+        call    draw_text
+        call    make_puzzle
+        ld      hl, inst_msg
+        call    draw_text
+m_loop: call    get_key
+        cp      a, $1c          ; '0' = quit
+        jr      z, m_quit
+        cp      a, $1d          ; '1' = restart
+        jr      z, m_start
+        sub     a, $26          ; < 'A'?
+        jr      c, m_loop
+        cp      a, $19          ; >= 'Z'?
+        jr      nc, m_loop
+        call    make_move
+        ;; Check for victory
+        ld      b, $19
+m_vclp: ld      a, b
+        dec     a
+        call    get_display_addr
+        ld      a, (hl)
+        and     $80
+        jr      z, m_loop       ; A light is still on
+        djnz    m_vclp
+
+        ;; Victory!
+        ld      hl, win_again_msg
+        call    draw_text
+m_won:  call    get_key
+        cp      a, $3e          ; 'Y'?
+        jr      z, m_start
+        cp      a, $33          ; 'N'?
+        jr      nz, m_won
+
+        ;; All done. Say goodbye properly.
+m_quit: ld      hl, ($400c)
+        ;; Home the cursor
+        inc     hl
+        ld      ($400e),hl
+        dec     hl
+        ;; Clear the screen to spaces
+        ld      bc, $1800
+        call    fill_lines
+
+        ;; Print farewell message
+        ld      hl, farewell_msg
+m_0:    ld      a, (hl)
+        cp      a, $ff
+        ret     z
+        push    hl
+        rst     $10
+        pop     hl
+        inc     hl
+        jr      m_0
+
+draw_board:
+        ;; Fill the screen with darkness
+        ld      hl, ($400c)
+        push    hl
+        ld      bc, $1880
+        call    fill_lines
+
+        ;; Draw out the letters
+        ld      b, $19
+db_0:   ld      a, b
+        dec     a
+        call    get_display_addr
+        add     a, $a6
+        ld      (hl), a
+        djnz    db_0
+        ;; Draw the grey border around the play area
+        pop     hl
+        ld      a, $88
+        ld      bc, $006c
+        add     hl, bc
+        push    hl
+        ld      de, $0210
+        ld      c, 1
+        call    db_1
+        pop     hl
+        ld      de, $0010
+        ld      c, $21
+        ;; Fall through
+db_1:   ld      b, $11
+db_2:   ld      (hl), a
+        add     hl, de
+        ld      (hl), a
+        sbc     hl, de
+        push    bc
+        ld      b, 0
+        add     hl, bc
+        pop     bc
+        djnz    db_2
+        ret
+
+        ;; HL: pointer to located text to draw.
+        ;;     Texts come in pairs.
+draw_text:
+        push    hl
+        ld      hl, ($400c)
+        ld      bc, $02b6
+        add     hl, bc
+        ld      bc, $0280
+        call    fill_lines
+        pop     hl
+        call    dt_0             ; Run the rest twice
+dt_0:   ld      e, (hl)
+        inc     hl
+        ld      d, (hl)
+        inc     hl
+        push    hl
+        ld      hl, ($400c)
+        add     hl, de
+        ld      d, h
+        ld      e, l
+        pop     hl
+        ld      a, $ff
+dt_1:   cp      a, (hl)
+        jr      z, dt_2
+        ldi
+        jr      dt_1
+dt_2:   inc     hl              ; Skip delimiter
+        ret
+
+make_move:
+        call    get_display_addr
+        call    flip_square
+        ld      bc, -$63
+        add     hl, bc
+        call    flip_square
+        ld      bc, $60
+        add     hl, bc
+        call    flip_square
+        ld      bc, $06
+        add     hl, bc
+        call    flip_square
+        ld      bc, $60
+        add     hl, bc
+        ;; Fall through to flip_square
+
+flip_square:
+        ld      a, (hl)
+        ;; Range check
+        and     a, $7f          ; remove reverse video status
+        cp      a, $26          ; <  "A"?
+        ret     c
+        cp      a, $3f          ; >= "Z"?
+        ret     nc
+        push    hl
+        ld      a, (hl)
+        ld      bc, -$22
+        add     hl, bc
+        ld      d, h
+        ld      e, l
+        ld      hl, on_border
+        add     a, $80
+        jr      c, fs_0
+        ld      hl, off_border
+fs_0:   ldi
+        ldi
+        ldi
+        call    fs_1            ; Next line
+        ldi
+        ld      (de), a
+        inc     de
+        ldi
+        call    fs_1
+        ldi
+        ldi
+        ldi
+        pop     hl
+        ret
+fs_1:   push    hl
+        ld      h, d
+        ld      l, e
+        ld      bc, $1e
+        add     hl, bc
+        ld      d, h
+        ld      e, l
+        pop     hl
+        ret
+
+        ;; HL: Destination address
+        ;;  B: Number of lines to fill
+        ;;  C: Character with which to fill
+fill_lines:
+        inc     hl
+        ld      a, (hl)
+        cp      a, $76
+        jr      z, fl_0
+        ld      (hl), c
+
+        jr      fill_lines
+fl_0:   djnz    fill_lines
+        ret
+
+get_display_addr:
+        push    af
+        push    bc
+
+        ld      hl, ($400c)
+        ld      bc, $00b0
+        add     hl, bc
+        ld      bc, $0063
+gda_lp: cp      a, $05
+        jr      c, gda_dn
+        add     hl, bc
+        sub     a, $05
+        jr      gda_lp
+gda_dn: ld      c, a            ; a = a * 3
+        add     a, a
+        add     a, c
+        ld      b, 0
+        ld      c, a
+        add     hl, bc          ; a is less than 16, so carry is clear
+
+        pop     bc
+        pop     af
+        ret
+
+get_key:
+        ;; Wait for no key, then wait for key
+        call    $02bb           ; KSCAN
+        inc     l
+        jr      nz, get_key
+gk_0:   call    rnd             ; step the RNG while waiting for a key
+        call    $02bb
+        inc     l
+        jr      z, gk_0
+        dec     l
+        ld      b, h
+        ld      c, l
+        call    $07bd           ; FINDCHR
+        ld      a, (hl)
+        ret
+
+make_puzzle:
+        ld      bc, 1000
+mp_0:   push    bc
+mp_1:   call    rnd
+        ld      a, h
+        and     a, $1f          ; Extract a 0-31 value
+        cp      a, $19          ; Reroll if it's 25-31
+        jr      nc, mp_1
+        call    make_move
+        pop     bc
+        dec     c               ; Apparently DEC BC doesn't set Z?
+        jr      nz, mp_0
+        djnz    mp_0
+        ret
+
+rnd_x:  defw    $0001
+rnd_y:  defw    $0001
+rnd:    ld      hl, (rnd_x)
+        ;; t = x ^ (x << 5)  [t = DE and HL]
+        ld      d, h
+        ld      e, l
+        ld      b, 5
+rnd_0:  sla     l
+        rl      h
+        djnz    rnd_0
+        ld      a, h
+        xor     a, d
+        ld      d, a
+        ld      h, a
+        ld      a, l
+        xor     a, e
+        ld      e, a
+        ld      l, a
+        ;; t = t ^ (t >>> 3) [t = DE]
+        ld      b, 3
+rnd_1:  srl     h
+        rr      l
+        djnz    rnd_1
+        ld      a, h
+        xor     a, d
+        ld      d, a
+        ld      a, l
+        xor     a, e
+        ld      e, a
+        ;; x = y
+        ld      hl, (rnd_y)
+        ld      (rnd_x), hl
+        ;; y = y ^ (y >>> 1) ^ t [t = DE, y = HL]
+        ld      b, h
+        ld      c, l
+        srl     h
+        rr      l
+        ld      a, b
+        xor     a, h
+        xor     a, d
+        ld      h, a
+        ld      a, c
+        xor     a, l
+        xor     a, e
+        ld      l, a
+        ld      (rnd_y), hl
+        ;; return y (still in HL)
+        ret
+
+;;; Positioned messages, for poking into place. $FF-terminated, and the
+;;; first two bytes indicate the offset into the display file to show
+;;; them.
+title_wait_msg:
+        defw    $002D
+        defb    $B1,$AE,$AC,$AD,$B9,$B8,$80,$80,$B4,$BA,$B9,$FF
+        defw    $02BF
+        defb    $B5,$B1,$AA,$A6,$B8,$AA,$80,$80,$BC,$A6,$AE,$B9,$9B,$9B,$9B,$FF
+
+inst_msg:
+        defw    $02BC
+        defb    $B5,$B7,$AA,$B8,$B8,$80,$B1,$AA,$B9,$B9,$AA,$B7,$B8,$80,$B9,$B4
+        defb    $80,$B2,$B4,$BB,$AA,$FF
+        defw    $02D8
+        defb    $9D,$80,$AB,$B4,$B7,$80,$B3,$AA,$BC,$80,$B5,$BA,$BF,$BF,$B1,$AA
+        defb    $80,$80,$80,$80,$80,$9C,$80,$B9,$B4,$80,$B6,$BA,$AE,$B9,$FF
+
+win_again_msg:
+        defw    $02BF
+        defb    $A8,$B4,$B3,$AC,$B7,$A6,$B9,$BA,$B1,$A6,$B9,$AE,$B4,$B3,$B8,$FF
+        defw    $02DF
+        defb    $B5,$B1,$A6,$BE,$80,$A6,$AC,$A6,$AE,$B3,$80,$90,$BE,$98,$B3,$91
+        defb    $8F,$FF
+
+;;; Unpositioned; this one is for printing with RST 10 after play.
+farewell_msg:
+        defb    $00,$00,$00,$00,$00,$00,$88,$88,$80,$80,$00,$31,$2E,$2C,$2D,$39
+        defb    $38,$00,$34,$3A,$39,$00,$80,$80,$88,$88,$76,$00,$00,$00,$27,$3A
+        defb    $32,$27,$2A,$37,$38,$2D,$34,$34,$39,$00,$38,$34,$2B,$39,$3C,$26
+        defb    $37,$2A,$1A,$00,$1E,$1C,$1D,$23,$76,$76,$76,$39,$2D,$26,$33,$30
+        defb    $38,$00,$2B,$34,$37,$00,$35,$31,$26,$3E,$2E,$33,$2C,$1B,$1B,$1b
+        defb    $76,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+        defb    $00,$16,$16,$32,$2E,$28,$2D,$26,$2A,$31,$00,$32,$26,$37,$39,$2E
+        defb    $33,$76,$FF
+
+on_border:
+        defb    $07,$03,$84,$05,$85,$82,$83,$81
+
+off_border:
+        defb    $80,$80,$80,$80,$80,$80,$80,$80
