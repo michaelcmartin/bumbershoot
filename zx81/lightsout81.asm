07a0acdd5cdba03ccdc32f8a16fdf6e78fee7b4f
Michael Martin <mcmartin@gmail.com>
2017-03-11 02:02:30 -0800

    Use the optimized implementation of the xorshift PRNG

--- zx81/lightsout81.asm
+++ zx81/lightsout81.asm
@@ -263,51 +263,48 @@ mp_1:   call    rnd
         djnz    mp_0
         ret
 
+rnd:    ;; t = x ^ (x << 5)  [t = DE]
+        defb    $11             ; LD DE, **
 rnd_x:  defw    $0001
-rnd_y:  defw    $0001
-rnd:    ld      hl, (rnd_x)
-        ;; t = x ^ (x << 5)  [t = DE and HL]
-        ld      d, h
-        ld      e, l
+        ld      a, e
+        ld      c, d
         ld      b, 5
-rnd_0:  sla     l
-        rl      h
+rnd_0:  sla     a
+        rl      d
         djnz    rnd_0
-        ld      a, h
-        xor     a, d
-        ld      d, a
-        ld      h, a
-        ld      a, l
         xor     a, e
         ld      e, a
-        ld      l, a
-        ;; t = t ^ (t >>> 3) [t = DE]
+        ld      a, c
+        xor     a, d
+        ld      d, a
+        ;; t ^= t >> 3  [t = DE]  [A is already D]
+        ld      c, e
         ld      b, 3
-rnd_1:  srl     h
-        rr      l
+rnd_1:  srl     a
+        rr      e
         djnz    rnd_1
-        ld      a, h
         xor     a, d
         ld      d, a
-        ld      a, l
+        ld      a, c
         xor     a, e
         ld      e, a
-        ;; x = y
-        ld      hl, (rnd_y)
+        ;; x = y        [t = DE, y = HL]
+        defb    $21             ; LD HL, **
+rnd_y:  defw    $0001
         ld      (rnd_x), hl
-        ;; y = y ^ (y >>> 1) ^ t [t = DE, y = HL]
-        ld      b, h
+        ;; y ^= (y >> 1) ^ t [t = DE, y = HL]
+        ld      a, h
         ld      c, l
-        srl     h
+        srl     a
         rr      l
-        ld      a, b
         xor     a, h
         xor     a, d
         ld      h, a
-        ld      a, c
-        xor     a, l
+        ld      a, l
+        xor     a, c
         xor     a, e
         ld      l, a
+        ;; Store out y
         ld      (rnd_y), hl
         ;; return y (still in HL)
         ret
