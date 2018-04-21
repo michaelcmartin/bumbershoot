d54c23e9dc0cf9b3521c14d476e5cf12e34b63b9
Michael Martin <mcmartin@gmail.com>
2018-04-21 16:56:19 -0700

    Create a scale with a 'grand piano' instrument preset

--- genesis/tools/ym2612_data.py
+++ genesis/tools/ym2612_data.py
@@ -11,8 +11,23 @@ def ym2612_vals(hz):
     while fnum >= 2048:
         fnum /= 2.0
         block += 1
-    return (block, int(round(fnum)))
+    fnum = int(round(fnum))
+    return (block << 11) | fnum
 
-print ym2612_hz(1083, 4) # ~ 440 Hz
-print ym2612_vals(440)   # Should give us 4, 1083 back
-print ym2612_vals(523)   # Should give us 4, 1287
+def freq(noteval, octave):
+    val = 4 * 12 + 10  # A-440
+    target = octave * 12 + noteval
+    step = 2.0 ** (1.0 / 12)
+    return ym2612_vals(440.0 * (step ** (target - val)))
+
+notes = [freq(x, 4) for x in [1, 3, 5, 6, 8, 10, 12, 13]]
+print "        defb    $3C,$21,$30,$71,$34,$0D,$38,$33,$3C,$01"
+print "        defb    $40,$23,$44,$2D,$48,$26,$4C,$00,$50,$5F"
+print "        defb    $54,$99,$58,$5F,$5C,$94,$60,$05,$64,$05"
+print "        defb    $68,$05,$6C,$07,$70,$02,$74,$02,$78,$02"
+print "        defb    $7C,$02,$80,$11,$84,$11,$88,$11,$8C,$A6"
+print "        defb    $90,$00,$94,$00,$98,$00,$9C,$00,$B0,$32"
+print "        defb    $B4,$C0,$A4,$%02X,$A0,$%02X,$28,$F0" % (notes[0] >> 8, notes[0] & 0xff)
+for note in notes[1:]:
+    print "        defb    $3C,$03,$A4,$%02X,$A0,$%02X,$28,$F0" % (note >> 8, note & 0xff)
+print "        defb    $00"
