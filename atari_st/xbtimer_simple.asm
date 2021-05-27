e32ff8606d048d9f1a117a38baa17094d84a9ae2
Michael Martin <mcmartin@gmail.com>
2021-05-26 23:54:55 -0700

    Split Xbtimer demo into GEMDOS and BIOS versions

--- atari_st/xbtimer_simple.asm
+++ atari_st/xbtimer_simple.asm
@@ -10,16 +10,31 @@
 	trap	#14			; in XBIOS
 	addq.l	#6,sp
 
-	move.l	#intro,a0		; Print greeting
+	move.l	#intro,a0
 	bsr	outstr
 
 lp:	move.l	#cr,a0			; Print header
 	bsr	outstr
 	move.l	counter,d0		; and counter value
 	bsr	outhex			; (updated by timer handler)
-	bra.s	lp			; looping forever
+	move.w	#2,-(sp)		; Check keyboard
+	move.w	#1,-(sp)		; Bconstat()
+	trap	#13			; BIOS
+	addq.l	#4,sp
+	tst.w	d0
+	beq.s	lp			; looping while no keys ready
 
-	move.l	#unreachable_msg,a0	; Can't happen!
+	move.l	#unconfig_tick,-(sp)
+	move.w	#38,-(sp)
+	trap	#14
+	addq.l	#6,sp
+
+	move.w	#2,-(sp)		; From keyboard
+	move.w	#2,-(sp)		; Bconin()
+	trap	#13			; BIOS
+	addq.l	#4,sp
+
+	move.l	#goodbye,a0
 	bsr	outstr
 	clr.w	-(sp)
 	trap	#1
@@ -37,17 +52,6 @@ tick:	addq.l	#1,counter		; Bump counter
 ;;;                Trashes a0-a2/d0-d2.
 ;;; ----------------------------------------------------------------------
 config_tick:
-	move.l	#-1,-(sp)		; Read vector
-	move.w	#$102,-(sp)		; for $102 (terminate)
-	move.w	#5,-(sp)		; Setexec()
-	trap	#13			; BIOS call
-	addq.l	#8,sp
-	move.l	d0,oldterm		; Save original terminate vector
-	move.l	#terminate,-(sp)	; And set the vector to our new one
-	move.w	#$102,-(sp)
-	move.w	#5,-(sp)
-	trap	#13
-	addq.l	#8,sp
 	move.l	#tick,-(sp)		; Configure timer interrupt...
 	move.w	#0,-(sp)		; 256 counts per call
 	move.w	#7,-(sp)		; delay mode, 200 clocks per count
@@ -58,10 +62,9 @@ config_tick:
 	rts
 
 ;;; ----------------------------------------------------------------------
-;;;   terminate: Custom terminate handler. Fix vectors, turn off timer,
-;;;              and proceed with exit.
+;;;   unconfig_tick: Fix vectors, acknowledge any possibly-pending IRQ.
 ;;; ----------------------------------------------------------------------
-terminate:
+unconfig_tick:
 	move.l	#tick,-(sp)		; Configure timer interrupt...
 	move.w	#0,-(sp)
 	move.w	#0,-(sp)		; Disable timer
@@ -70,28 +73,7 @@ terminate:
 	trap	#14			; XBIOS
 	add.l	#12,sp
 	bclr.b	#5,$fffa0f		; Ack maybe-pending IRQ
-	move.l	oldterm,-(sp)		; Restore original terminate vector
-	move.w	#$102,-(sp)
-	move.w	#5,-(sp)
-	trap	#13
-	addq.l	#8,sp
-	;; We could return to the original dispatcher here, but let's
-	;; print a nice message and quit cleanly so we don't give an
-	;; error message.
-	;; We can't use GEMDOS (and thus Cconws()) inside the handler, so we
-	;; loop with Bconout() instead
-	move.l	#goodbye,a3
-	subq.l	#6,sp
-.bye:	clr.w	4(sp)
-	move.b	(a3)+,5(sp)
-	beq.s	.done
-	move.w	#2,2(sp)
-	move.w	#3,(sp)
-	trap	#13
-	bra.s	.bye
-.done:	addq.l	#4,sp
-	clr.w	(sp)
-	trap	#1
+	rts
 
 ;;; ----------------------------------------------------------------------
 ;;;   outhex: Print the 32-bit number in d0 to console.
@@ -111,8 +93,9 @@ outhex:	move.w	#0,-(sp)		; Push 0 terminator on stack
 .out:	tst.w	(sp)			; Loop until we hit the 0 terminator
 	beq.s	.done
 	move.w	#2,-(sp)		; And print the characters with
-	trap	#1			; Cconout (GEMDOS)
-	addq.l	#4,sp
+	move.w	#3,-(sp)		; Bconout()
+	trap	#13			; BIOS
+	addq.l	#6,sp
 	bra.s	.out
 .done:	addq.l	#2,sp			; Pop the 0 terminator
 	rts				; and return
@@ -121,23 +104,26 @@ outhex:	move.w	#0,-(sp)		; Push 0 terminator on stack
 ;;;   outstr: Print the null-terminated string in a0 to console.
 ;;;           Trashes a0-a2/d0-d2.
 ;;; ----------------------------------------------------------------------
-outstr:	move.l	a0,-(sp)		; Address of string
-	move.w	#9,-(sp)		; Cconws()
-	trap	#1			; GEMDOS
-	addq.l	#6,sp
+outstr:	move.l	a3,-(sp)
+	move.l	a0,a3
+	subq.l	#6,sp
+.lp:	clr.w	4(sp)
+	move.b	(a3)+,5(sp)
+	beq.s	.done
+	move.w	#2,2(sp)
+	move.w	#3,(sp)
+	trap	#13
+	bra.s	.lp
+.done:	addq.l	#6,sp
+	move.l	(sp)+,a3
 	rts
 
 	data
 counter:
 	dc.l	0
 
-intro:	dc.b	"MFP Timer Demonstration",13,10,"Press Ctrl-C to exit",13,10,0
+intro:	dc.b	"MFP Timer Demonstration",13,10
+	dc.b	"Press any key to exit",13,10,0
 cr:	dc.b	13,"Timer: ",0
-unreachable_msg:
-	dc.b	13,10,"Big yikes!",13,10
-	dc.b	"Somehow reached unreachable code!",13,10,0
 goodbye:
 	dc.b	13,10,"Timer interrupted. Shutting down.",13,10,0
-	bss
-oldterm:
-	ds.l	1
