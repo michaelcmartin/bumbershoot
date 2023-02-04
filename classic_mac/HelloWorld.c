fce8dc0682d95da965e32535ad8a61bba428f691
Michael Martin <mcmartin@gmail.com>
2023-02-03 19:18:42 -0800

    Handle Update, Drag, and Close events

--- classic_mac/HelloWorld.c
+++ classic_mac/HelloWorld.c
@@ -9,9 +9,12 @@ QDGlobals qd;
 
 void main (void)
 {
-	WindowPtr wnd;
-	Rect windowRect;
+	WindowPtr wnd, clickWnd;
+	Rect windowRect, dragRect;
 	short fontNumber;
+	EventRecord myEvent;
+	int done = 0;
+	int clickLoc = 0;
 
 	InitGraf(&qd.thePort);
 	InitFonts();
@@ -21,25 +24,54 @@ void main (void)
 	InitDialogs(nil);
 	InitCursor();
 
-	windowRect.left = 10;
-	windowRect.top = 40;
-	windowRect.right = 310;
-	windowRect.bottom = 240;
+	SetRect(&windowRect, 10, 40, 310, 240);
+	SetRect(&dragRect, -32767, -32767, 32767, 32767);
 
-	wnd = NewCWindow(nil, &windowRect, "\pHello, World", true,
+	wnd = NewWindow(nil, &windowRect, "\pHello, World", true,
 			noGrowDocProc, (WindowPtr)-1L, true, 0);
 
-	SetPort(wnd);
-	GetFNum("\pChicago", &fontNumber);
-	TextFont(fontNumber);
-	TextSize(12);
-	MoveTo(10, 12);
-	DrawString("\pHello, World, from Bumbershoot Software!");
-
-	do {
+	while (!done) {
 		SystemTask();
-	} while (!Button());
+		GetNextEvent(everyEvent, &myEvent);
+		switch (myEvent.what) {
+		case mouseDown:
+			clickLoc = FindWindow(myEvent.where, &clickWnd);
+			switch (clickLoc) {
+			case inSysWindow:
+				SystemClick(&myEvent, clickWnd);
+				break;
+			case inContent:
+				if (clickWnd != FrontWindow()) {
+					SelectWindow(clickWnd);
+				}
+				break;
+			case inDrag:
+				DragWindow(clickWnd, myEvent.where, &dragRect);
+				break;
+			case inGoAway:
+				if (TrackGoAway(clickWnd, myEvent.where)) {
+					done = 1;
+				}
+				break;
+			default:
+				break;
+			}
+			break;
+		case updateEvt:
+			SetPort(wnd);
+			BeginUpdate(wnd);
+			GetFNum("\pChicago", &fontNumber);
+			TextFont(fontNumber);
+			TextSize(12);
+			MoveTo(10, 12);
+			EraseRect(&wnd->portRect);
+			DrawString("\pHello, World, from Bumbershoot Software!");
+			EndUpdate(wnd);
+			break;
+		default:
+			break;
+		}
+	}
 
 	DisposeWindow(wnd);
 }
-
