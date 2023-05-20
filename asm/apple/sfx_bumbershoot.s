33de059e07ed4c8f1f430c826e2cfbc7b632cfef
Michael Martin <mcmartin@gmail.com>
2023-05-20 16:34:57 -0700

    IIgs sound playback

---
+++ asm/apple/sfx_bumbershoot.s
@@ -0,0 +1,9 @@
+            REL
+            LNK     WOW.L
+
+            ENT     SFXLEN
+SFX         ENT
+            PUTBIN  bumber_gs.bin
+SFXEND
+
+SFXLEN      EQU     SFXEND-SFX
---
+++ asm/apple/sfx_wow.s
@@ -0,0 +1,9 @@
+            REL
+            LNK     WOW.L
+
+            ENT     SFXLEN
+SFX         ENT
+            PUTBIN  wow_gs.bin
+SFXEND
+
+SFXLEN      EQU     SFXEND-SFX
