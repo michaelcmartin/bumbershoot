7f135c9d5ff4581d47e2349b7d4b4dff772dac30
Michael Martin <mcmartin@gmail.com>
2024-02-19 18:28:36 -0800

    Simplify graphics data format

--- trs80/coco/liberty.s
+++ trs80/coco/liberty.s
@@ -1,22 +1,20 @@
 	ORG	$4000
 
 	CLRA
-	CLRB
-	STD	GFXIDX
 	JSR	PCOLOR
 	LDA	#$FF
 	JSR	PCLS
 	JSR	PMODE
 
-	BSR	GFXREAD
+	LDX	#GFX
+	LDD	,X++
 	JSR	PSET
 
-PLOTLP	LDX	GFXIDX
-	CMPX	#GFXSZ
+PLOTLP	CMPX	#GFXEND
 	BEQ	DOPAINT
-	BSR	GFXREAD
+	LDD	,X++
 	BNE	1F
-	BSR	GFXREAD			; Y was 0, start new line sequence
+	LDD	,X++			; Start new line sequence
 	JSR	PSET			; with fresh read
 	BRA	PLOTLP
 1	JSR	PLINE			; Y was not 0, continue line
@@ -47,17 +45,6 @@ DOPAINT	LDD	#$6451
 	JSR	[$A002]
 	BRA	1B
 1	RTS
-
-	;; TODO: Combine and precenter graphics so this can just be
-	;;       an LDD ,X++ instruction and some later tests.
-GFXREAD	LDX	GFXIDX
-	LDA	YGFX,X
-	LDB	XGFX,X
-	LEAX	1,X
-	STX	GFXIDX
-	ADDB	#7			; Center image
-	TSTA
-	RTS
 
 PAINT	JSR	POINT			; Already filled?
 	BEQ	2F			; If so, abort
@@ -97,48 +84,36 @@ PAINT	JSR	POINT			; Already filled?
 	INCLUDE	"bitmap.s"
 
         ;; Graphics data
-GFXIDX	FDB	0
-XGFX	FCB	$2f,$40,$4b,$6d,$7a,$8a,$91,$88,$83,$72,$67,$5c
-	FCB	$56,$56,$5d,$5f,$65,$65,$68,$68,$6b,$6b,$6d,$71
-	FCB	$71,$6d,$70,$76,$78,$81,$7f,$7c,$7a,$77,$74,$74
-	FCB	$75,$72,$6c,$5f,$58,$57,$5e,$65,$75,$7e,$91,$96
-	FCB	$9c,$9d,$a2,$a4,$a5,$a4,$a2,$a0,$9c,$9b,$98,$9c
-	FCB	$98,$9c,$9c,$a4,$c2,$ba,$bc,$b8,$b3,$b1,$a8,$ac
-	FCB	$a4,$a4,$a7,$d8,$ac,$ac,$e9,$a7,$9e,$cc,$8c,$7e
-	FCB	$75,$6d,$5d,$1d,$4c,$4c,$44,$01,$3f,$3f,$10,$43
-	FCB	$48,$3f,$2f,$00,$14,$40,$3f,$00,$04,$40,$44,$00
-	FCB	$1f,$58,$56,$5c,$62,$00,$7d,$71,$87,$95,$95,$cc
-	FCB	$00,$9f,$9f,$a8,$a9,$e8,$00,$ab,$ab,$d8,$00,$a7
-	FCB	$a1,$9e,$a6,$a3,$9a,$97,$9c,$98,$91,$8b,$8e,$88
-	FCB	$86,$7d,$7d,$78,$78,$71,$71,$6c,$6d,$64,$60,$5c
-	FCB	$5f,$58,$51,$4e,$54,$58,$64,$54,$4f,$53,$5e,$68
-	FCB	$70,$76,$85,$90,$98,$9c,$a0,$a3,$a4,$a7,$00,$80
-	FCB	$80,$82,$89,$90,$94,$92,$8f,$8d,$8b,$8d,$8b,$88
-	FCB	$87,$85,$85,$80,$00,$4c,$50,$52,$52,$4c,$00,$68
-	FCB	$6a,$73,$76,$7b,$7c,$81,$88,$81,$7d,$7b,$78,$76
-	FCB	$71,$70,$71,$7e,$7a,$78,$74,$71,$6d,$69,$69
-
-YGFX	FCB	$78,$90,$ab,$b1,$ae,$99,$8b,$92,$94,$95,$91,$87
-	FCB	$76,$6d,$6d,$6a,$6a,$68,$68,$6a,$6a,$68,$68,$6d
-	FCB	$73,$7d,$7f,$81,$81,$7f,$7d,$7d,$7d,$7d,$7d,$73
-	FCB	$68,$63,$63,$63,$65,$62,$5c,$5b,$53,$59,$5f,$61
-	FCB	$6b,$70,$74,$74,$76,$77,$77,$7a,$7e,$82,$84,$8d
-	FCB	$a5,$a5,$af,$b3,$b3,$a2,$a0,$9e,$86,$7f,$6f,$6b
-	FCB	$5e,$54,$4f,$4e,$45,$43,$3b,$3b,$37,$12,$2f,$2d
-	FCB	$0f,$2e,$2f,$12,$35,$37,$3b,$3b,$43,$45,$4f,$4f
-	FCB	$55,$6c,$78,$00,$4e,$4a,$44,$00,$3c,$3f,$3c,$00
-	FCB	$12,$33,$38,$37,$2e,$00,$2d,$33,$35,$39,$31,$12
-	FCB	$00,$38,$3e,$44,$3e,$3b,$00,$43,$4a,$4e,$00,$69
-	FCB	$5c,$4e,$48,$45,$4b,$49,$40,$3e,$47,$46,$3c,$3a
-	FCB	$45,$44,$39,$39,$44,$45,$3a,$3a,$44,$44,$3b,$3c
-	FCB	$45,$47,$3e,$40,$4a,$4a,$47,$4e,$55,$58,$59,$55
-	FCB	$51,$4d,$58,$5c,$5d,$5e,$62,$66,$6a,$6a,$00,$68
-	FCB	$67,$64,$63,$64,$68,$6a,$6a,$68,$68,$68,$68,$69
-	FCB	$68,$68,$6a,$68,$00,$94,$81,$81,$95,$94,$00,$86
-	FCB	$86,$83,$83,$84,$83,$84,$86,$86,$86,$86,$86,$86
-	FCB	$86,$87,$89,$8a,$8c,$8c,$8b,$8c,$89,$86,$86
-
-GFXSZ	EQU	YGFX-XGFX
+GFX	FDB	$783D,$904E,$AB59,$B17B,$AE88,$9998,$8B9F,$9296
+	FDB	$9491,$9580,$9175,$876A,$7664,$6D64,$6D6B,$6A6D
+	FDB	$6A73,$6873,$6876,$6A76,$6A79,$6879,$687B,$6D7F
+	FDB	$737F,$7D7B,$7F7E,$8184,$8186,$7F8F,$7D8D,$7D8A
+	FDB	$7D88,$7D85,$7D82,$7382,$6883,$6380,$637A,$636D
+	FDB	$6566,$6265,$5C6C,$5B73,$5383,$598C,$5F9F,$61A4
+	FDB	$6BAA,$70AB,$74B0,$74B2,$76B3,$77B2,$77B0,$7AAE
+	FDB	$7EAA,$82A9,$84A6,$8DAA,$A5A6,$A5AA,$AFAA,$B3B2
+	FDB	$B3D0,$A2C8,$A0CA,$9EC6,$86C1,$7FBF,$6FB6,$6BBA
+	FDB	$5EB2,$54B2,$4FB5,$4EE6,$45BA,$43BA,$3BF7,$3BB5
+	FDB	$37AC,$12DA,$2F9A,$2D8C,$0F83,$2E7B,$2F6B,$122B
+	FDB	$355A,$375A,$3B52,$3B0F,$434D,$454D,$4F1E,$4F51
+	FDB	$5556,$6C4D,$783D,$0000,$4E22,$4A4E,$444D,$0000
+	FDB	$3C12,$3F4E,$3C52,$0000,$122D,$3366,$3864,$376A
+	FDB	$2E70,$0000,$2D8B,$337F,$3595,$39A3,$31A3,$12DA
+	FDB	$0000,$38AD,$3EAD,$44B6,$3EB7,$3BF6,$0000,$43B9
+	FDB	$4AB9,$4EE6,$0000,$69B5,$5CAF,$4EAC,$48B4,$45B1
+	FDB	$4BA8,$49A5,$40AA,$3EA6,$479F,$4699,$3C9C,$3A96
+	FDB	$4594,$448B,$398B,$3986,$4486,$457F,$3A7F,$3A7A
+	FDB	$447B,$4472,$3B6E,$3C6A,$456D,$4766,$3E5F,$405C
+	FDB	$4A62,$4A66,$4772,$4E62,$555D,$5861,$596C,$5576
+	FDB	$517E,$4D84,$5893,$5C9E,$5DA6,$5EAA,$62AE,$66B1
+	FDB	$6AB2,$6AB5,$0000,$688E,$678E,$6490,$6397,$649E
+	FDB	$68A2,$6AA0,$6A9D,$689B,$6899,$689B,$6899,$6996
+	FDB	$6895,$6893,$6A93,$688E,$0000,$945A,$815E,$8160
+	FDB	$9560,$945A,$0000,$8676,$8678,$8381,$8384,$8489
+	FDB	$838A,$848F,$8696,$868F,$868B,$8689,$8686,$8684
+	FDB	$867F,$877E,$897F,$8A8C,$8C88,$8C86,$8B82,$8C7F
+	FDB	$897B,$8677,$8677
+GFXEND
 
 CREDITS	FCB	13,"    = PORTRAIT OF LIBERTY =",13,13
 	FCB	" ORIGINAL BY JOHN JAINSCHIGG",13
