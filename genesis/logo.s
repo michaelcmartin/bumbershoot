1ef0a626b6e245bb2d4b81e3d4883a11a30b0478
Michael Martin <mcmartin@gmail.com>
2025-06-19 16:17:54 -0700

    Fade out logo

--- genesis/logo.s
+++ genesis/logo.s
@@ -5,7 +5,7 @@
 ;;; includes hit
 
 BumbershootLogo:
-        movem.l d2/a2-a3,-(sp)
+        movem.l d2-d3/a2-a3,-(sp)
 
         lea     .logodata,a0
         lea     $ff0000,a1
@@ -51,15 +51,81 @@ BumbershootLogo:
         ;; Wait 240 frames or until START is pressed
         move.w  #240,d2
 .v1:    move.w  (a3),d0
-        btst    #3,d0          ; Wait for no VBLANK
+        btst    #3,d0                   ; Wait for no VBLANK
         bne.s   .v1
 .v2:    move.w  (a3),d0
-        btst    #3,d0          ; Wait for VBLANK
+        btst    #3,d0                   ; Wait for VBLANK
         beq.s   .v2
         bsr     ReadJoy1
-        btst    #7,d0           ; Start pressed?
-        bne.s   .skip           ; If so, end immediately
+        btst    #7,d0                   ; Start pressed?
+        bne     .skip                   ; If so, end immediately
         dbra    d2,.v1
+
+        ;; --- Fade out logo ---
+
+        ;; Step 1: split out each color into three words for R G B
+        lea     $ff0000,a0
+        lea     64(a0),a1
+        moveq   #31,d0
+.fade1: move.w  (a0)+,d1
+        move.w  d1,d2
+        and.w   #$00f,d2
+        lsr.w   #1,d2
+        move.w  d2,(a1)+
+        move.w  d1,d2
+        and.w   #$0f0,d2
+        lsr.w   #5,d2
+        move.w  d2,(a1)+
+        move.w  d1,d2
+        and.w   #$f00,d2
+        lsr.w   #8,d2
+        lsr.w   #1,d2
+        move.w  d2,(a1)+
+        dbra    d0,.fade1
+        ;; Step 2: Create "fixed point" buffers for each color to hold our
+        ;; full intermediate colors. Note that at this point a0 is where a1
+        ;; started, and a1 is our new destination
+        moveq   #95,d0
+.fade2: move.w  (a0)+,d1
+        lsl.w   #6,d1
+        move.w  d1,(a1)+
+        dbra    d0,.fade2
+        ;; Step 3: 60-frame loop where we repeatedly subtract the step 1
+        ;; buffer from the step 2 buffer, reassemble the palette, and blit it
+        ;; to CRAM
+        moveq   #59,d3
+.fade3: lea     $ff0040,a0              ; 3a: Subtract 1st buffer from 2nd
+        lea     192(a0),a1
+        moveq   #95,d0
+.f3a:   move.w  (a0)+,d1
+        sub.w   d1,(a1)+
+        dbra    d0,.f3a
+.f3b1:  move.w  (a3),d0                 ; 3b: Wait for VBLANK, abort if START
+        btst    #3,d0
+        bne.s   .f3b1
+.f3b2:  move.w  (a3),d0
+        btst    #3,d0
+        beq.s   .f3b2
+        bsr     ReadJoy1
+        btst    #7,d0
+        bne.s   .skip
+        lea     $ff0100,a0              ; 3c: Reassemble palette and blit to CRAM
+        move.l  #$c0000000,(a3)
+        moveq   #31,d0
+.f3c:   move.w  (a0)+,d2
+        lsr.w   #5,d2
+        and.w   #$00e,d2
+        move.w  (a0)+,d1
+        lsr.w   #1,d1
+        and.w   #$0e0,d1
+        or.w    d1,d2
+        move.w  (a0)+,d1
+        lsl.w   #3,d1
+        and.w   #$e00,d1
+        or.w    d1,d2
+        move.w  d2,(a2)
+        dbra    d0,.f3c
+        dbra    d3,.fade3
 
         ;; Stop playback by taking over the Z80 bus
 .skip:  move.w  #256,$a11100
@@ -72,7 +138,14 @@ BumbershootLogo:
 .cls:   move.l  d0,(a2)
         dbra    d1,.cls
 
-        movem.l (sp)+,d2/a2-a3
+        ;; Restore palette in CRAM
+        move.l  #$c0000000,(a3)
+        lea     $ff0000,a0
+        moveq   #15,d0
+.pfix:  move.l  (a0)+,(a2)
+        dbra    d0,.pfix
+
+        movem.l (sp)+,d2-d3/a2-a3
         rts
 
 .logodata:
