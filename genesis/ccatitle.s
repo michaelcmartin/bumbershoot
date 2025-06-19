f71b7758b437d5853866ba342c98cabfc3ec99a5
Michael Martin <mcmartin@gmail.com>
2025-06-19 15:01:26 -0700

    Skip logo screen with START

--- genesis/ccatitle.s
+++ genesis/ccatitle.s
@@ -1,51 +1,55 @@
 BumbershootTitle:
-	move.l	a2,-(a7)
-	lea	$c00000,a0		; Set up VDP registers
-	lea	4(a0),a2
-	lea	.title,a1		; Set up messages
-	move.w	#$8104,(a2)		; Disable display
-.blit:	move.l	a1,d0			; Is address even?
+	movem.l	a2-a3,-(a7)
+	lea	$c00000,a2		; Set up VDP registers where function
+	lea	4(a2),a3		; calls won't clobber them
+
+	bsr	ReadJoy1		; Skip title if START already down
+	btst	#7,d0
+	bne.s	.wait2
+
+	lea	.title,a0		; Set up messages
+	move.w	#$8104,(a3)		; Disable display
+.blit:	move.l	a0,d0			; Is address even?
 	btst	#0,d0
 	beq.s	.ok
-	addq	#1,a1			; If not, advance one more
-.ok:	move.l	(a1)+,d0		; Load next VRAM control
+	addq	#1,a0			; If not, advance one more
+.ok:	move.l	(a0)+,d0		; Load next VRAM control
 	beq.s	.ready			; Stop if done
-	move.l	d0,(a2)
+	move.l	d0,(a3)
 	moveq	#0,d0
-.line:	move.b	(a1)+,d0		; Load next byte
+.line:	move.b	(a0)+,d0		; Load next byte
 	beq.s	.blit			; Finish string if null
-	move.w	d0,(a0)			; Write to VRAM
+	move.w	d0,(a2)			; Write to VRAM
 	bra.s	.line
-.ready:	move.w	#$8144,(a2)		; Re-enable display
+.ready:	move.w	#$8144,(a3)		; Re-enable display
 
-.wait:	move.w	(a2),d0
+.wait:	move.w	(a3),d0
 	btst	#3,d0			; Wait for no VBLANK
 	bne.s	.wait
-.v:	move.w	(a2),d0
+.v:	move.w	(a3),d0
 	btst	#3,d0			; Wait for VBLANK
 	beq.s	.v
 	bsr	ReadJoy1
 	btst	#7,d0			; Start pressed?
 	beq.s	.wait			; If not, keep waiting
 
-.wait2:	move.w	(a2),d0
+.wait2:	move.w	(a3),d0
 	btst	#3,d0			; Wait for no VBLANK
 	bne.s	.wait2
-.v2:	move.w	(a2),d0
+.v2:	move.w	(a3),d0
 	btst	#3,d0			; Wait for VBLANK
 	beq.s	.v2
 	bsr	ReadJoy1
 	btst	#7,d0			; Start pressed?
 	bne.s	.wait2			; If so, keep waiting
 
-	move.l	#$40000003,(a2)
-	subq	#4,a2
+	move.l	#$40000003,(a3)
 	move.w	#2047,d1
 	moveq	#$00,d0
 .cls:	move.l	d0,(a2)
 	dbra	d1,.cls
 
-	move.l	(a7)+,a2
+	movem.l	(a7)+,a2-a3
 	rts
 
 .title:	dc.l	$41940003
--- genesis/logo.s
+++ genesis/logo.s
@@ -5,39 +5,39 @@
 ;;; includes hit
 
 BumbershootLogo:
-        move.l  a2,-(sp)
+        movem.l d2/a2-a3,-(sp)
 
         lea     .logodata,a0
         lea     $ff0000,a1
         bsr.s   lz4dec                  ; Puts amount uncompressed in d0
 
-        lea     $C00000,a0
-        lea     4(a0),a2
+        lea     $C00000,a2
+        lea     4(a2),a3
         lea     $ff0000,a1
 
         ;; Load palette
-        move.w  #$8f02,(a2)             ; VDP increment = 2
-        move.l  #$c0000000,(a2)         ; Write to CRAM 0
+        move.w  #$8f02,(a3)             ; VDP increment = 2
+        move.l  #$c0000000,(a3)         ; Write to CRAM 0
         moveq   #15,d1
-.pallp: move.l  (a1)+,(a0)
+.pallp: move.l  (a1)+,(a2)
         dbra    d1,.pallp
 
         ;; Load patterns
         sub.l   #$3040,d0               ; Total pattern bytes in d0
         lsr.l   #2,d0                   ; Total pattern longwords
         subq    #1,d0                   ; Adjustment for dbra
-        move.l  #$40000000,(a2)         ; Write to VRAM $0000
-.patlp: move.l  (a1)+,(a0)
+        move.l  #$40000000,(a3)         ; Write to VRAM $0000
+.patlp: move.l  (a1)+,(a2)
         dbra    d0,.patlp
 
         ;; Load nametables
-        move.l  #$40000003,(a2)         ; Write to VRAM $C000
+        move.l  #$40000003,(a3)         ; Write to VRAM $C000
         move.w  #3071,d0
-.namlp: move.l  (a1)+,(a0)
+.namlp: move.l  (a1)+,(a2)
         dbra    d0,.namlp
 
         ;; Enable the display
-        move.w  #$8144,(a2)
+        move.w  #$8144,(a3)
 
         ;; Set up the sample player
         bsr     SetupDAC
@@ -48,25 +48,31 @@ BumbershootLogo:
         bsr     PlaySample
         addq    #6,sp
 
-        ;; Wait 240 frames
-        move.w  #240,d1
-.v1:    move.w  (a2),d0
+        ;; Wait 240 frames or until START is pressed
+        move.w  #240,d2
+.v1:    move.w  (a3),d0
         btst    #3,d0          ; Wait for no VBLANK
         bne.s   .v1
-.v2:    move.w  (a2),d0
+.v2:    move.w  (a3),d0
         btst    #3,d0          ; Wait for VBLANK
         beq.s   .v2
-        dbra    d1,.v1
+        bsr     ReadJoy1
+        btst    #7,d0           ; Start pressed?
+        bne.s   .skip           ; If so, end immediately
+        dbra    d2,.v1
+
+        ;; Stop playback by taking over the Z80 bus
+.skip:  move.w  #256,$a11100
+        move.w  #256,$a11200
 
         ;; Clear screen
-        move.l  #$40940003,(a2)
-        subq    #4,a2
+        move.l  #$40940003,(a3)
         move.w  #3071,d1
         moveq   #$00,d0
 .cls:   move.l  d0,(a2)
         dbra    d1,.cls
 
-        move.l  (sp)+,a2
+        movem.l (sp)+,d2/a2-a3
         rts
 
 .logodata:
