ddcf46688d869f830dc411b6f7bb8800418bad9b
Michael Martin <mcmartin@gmail.com>
2025-07-27 21:11:19 -0700

    MSX1 port of shooting gallery

---
+++ msxetc/msx/gallery.asm
@@ -0,0 +1,136 @@
+;;;----------------------------------------------------------------------
+;;;  Shooting Gallery Shell: MSX edition
+;;;  See ../gallerycore.asm file for platform-independent logic.
+;;;----------------------------------------------------------------------
+
+	include	"msxbios.asm"
+
+	org	$8000
+	map	$e000
+	dw	$4241,INIT,0,0,0,0,0,0
+
+INIT:	ld	de,init_reg		; Init VDP registers
+	ld	c,0
+.reglp:	ld	a,(de)
+	inc	de
+	ld	b,a
+	call	WRTVDP
+	inc	c
+	ld	a,c
+	cp	8
+	jr	nz,.reglp
+	;; Draw static screen
+	ld	a,$00			; Clear lower 4KB of VRAM
+	ld	bc,$1000
+	ld	hl,0
+	call	FILVRM
+	ld	a,$07			; Draw divider line
+	ld	bc,$0020
+	ld	hl,$0020
+	call	FILVRM
+	ld	a,$68			; Draw ground
+	ld	bc,$0060
+	ld	hl,$02a0
+	call	FILVRM
+	ld	a,$f0			; Initialize Color RAM
+	ld	bc,$0020		; Most everything is white on black
+	ld	hl,$0380
+	call	FILVRM
+	ld	a,$22			; Ground is green
+	ld	hl,$38d
+	call	WRTVRM
+	ld	hl,initial_gfx
+	ld	bc,$000b		; Score display
+	ld	de,$0014
+	call	LDIRVM
+	;; Initialize graphics patterns
+	ld	hl,initial_gfx+$b
+	ld	bc,$0088		; Tile graphics
+	ld	de,$0808
+	call	LDIRVM
+	ld	hl,initial_gfx+$b+$88
+	ld	bc,$0018		; Sprite graphics
+	ld	de,$0b00
+	call	LDIRVM
+	ld	hl,initial_sprattr
+	ld	bc,$0030		; Load sprite attrs to CPU RAM
+	ld	de,sprattrs
+	ldir
+	ld	bc,$0031		; Then blit them to VRAM
+	ld	de,$0300
+	ld	hl,initial_sprattr
+	call	LDIRVM
+	ld	bc,$e101
+	call	WRTVDP
+
+	ld	hl,0
+	ld	(score),hl
+
+mainlp:	ld	hl,JIFFY
+	ld	a,(hl)
+1	halt
+	cp	(hl)
+	jr	z,1b
+
+	ld	a,(STATFL)
+	and	$20
+	ld	(collision),a
+	call	irq
+	jr	mainlp
+
+blit_sprites:
+	ld	hl,sprattrs
+	ld	de,$0300
+	ld	bc,$0030
+	call	LDIRVM
+	ret
+
+blit_score:
+	ld	hl,scorebuf
+	ld	de,$001b
+	ld	bc,$0004
+	call	LDIRVM
+	ret
+
+read_joystick:
+	push	bc
+	push	de
+	xor	a
+	ld	d,a
+	call	GTTRIG
+	and	a
+	jr	nz,1f
+	inc	a
+	call	GTTRIG
+	and	a
+	jr	nz,1f
+	ld	a,3
+	call	GTTRIG
+1	ld	(fire),a
+	xor	a
+	call	GTSTCK
+	and	a
+	jr	nz,1f
+	inc	a
+	call	GTSTCK
+1	add	a
+	ld	e,a
+	ld	hl,directions
+	add	hl,de
+	ld	a,(hl)
+	inc	hl
+	ld	h,(hl)
+	ld	l,a
+	pop	de
+	pop	bc
+	ret
+
+init_reg:
+	db	$00,$80,$00,$0e,$01,$06,$01,$f1
+
+directions:
+	dw	$0000,$00fe,$02fe,$0200,$0202,$0002,$fe02,$fe00,$fefe
+
+	include	"../gallerycore.asm"
+
+	ds	$a000-$,$ff
